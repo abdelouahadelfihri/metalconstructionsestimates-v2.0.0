348515a0e2781b5afc41ac241970f78630bde68a
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-05-04 18:33:36 +0100

    updates to AddEstimateLine.java

--- app/src/main/java/com/example/metalconstructionsestimates/modules/estimateslines/AddEstimateLine.java
+++ app/src/main/java/com/example/metalconstructionsestimates/modules/estimateslines/AddEstimateLine.java
@@ -381,9 +381,7 @@ public class AddEstimateLine extends AppCompatActivity {
             }
             @Override
             public void afterTextChanged(Editable s) {
-                TextInputEditText steelTypeTextInputEditText = findViewById(R.id.steelTypeEditText_add_estimate_line);
                 TextInputEditText weightTextInputEditText = findViewById(R.id.weightEditText_add_estimate_line);
-                TextInputEditText lengthTextInputEditText = findViewById(R.id.lengthEditText_add_estimate_line);
                 TextInputEditText widthTextInputEditText = findViewById(R.id.widthEditText_add_estimate_line);
                 TextInputEditText heightTextInputEditText = findViewById(R.id.heightEditText_add_estimate_line);
                 TextInputEditText quantityTextInputEditText = findViewById(R.id.quantityEditText_add_estimate_line);
@@ -500,10 +498,8 @@ public class AddEstimateLine extends AppCompatActivity {
             }
             @Override
             public void afterTextChanged(Editable s) {
-                TextInputEditText steelTypeTextInputEditText = findViewById(R.id.steelTypeEditText_add_estimate_line);
                 TextInputEditText weightTextInputEditText = findViewById(R.id.weightEditText_add_estimate_line);
                 TextInputEditText lengthTextInputEditText = findViewById(R.id.lengthEditText_add_estimate_line);
-                TextInputEditText widthTextInputEditText = findViewById(R.id.widthEditText_add_estimate_line);
                 TextInputEditText heightTextInputEditText = findViewById(R.id.heightEditText_add_estimate_line);
                 TextInputEditText quantityTextInputEditText = findViewById(R.id.quantityEditText_add_estimate_line);
                 TextInputEditText totalTextInputEditText = findViewById(R.id.totalEditText_add_estimate_line);
@@ -584,16 +580,15 @@ public class AddEstimateLine extends AppCompatActivity {
             }
             @Override
             public void afterTextChanged(Editable s) {
-
-                TextInputEditText weightTextInputEditText = findViewById(R.id.textInputEditText_steel_weight_add_estimate_line);
-                TextInputEditText lengthTextInputEditText = estimateLinesLengthWidthHeight.getTextInputEditTextLength();
-                TextInputEditText widthTextInputEditText = estimateLinesLengthWidthHeight.getTextInputEditTextWidth();
-                TextInputEditText quantityTextInputEditText = findViewById(R.id.textInputEditText_quantity_add_estimate_line);
-                TextInputEditText totalTextInputEditText = findViewById(R.id.textInputEditText_total_add_estimate_line);
-                TextInputEditText marginTextInputEditText = findViewById(R.id.textInputEditText_margin_add_estimate_line);
-                TextInputEditText netQuantityPlusMarginTextInputEditText = findViewById(R.id.textInputEditText_net_quantity_plus_margin_add_estimate_line);
-                TextInputEditText unitPriceTextInputEditText = findViewById(R.id.textInputEditText_unit_price_add_estimate_line);
-                TextInputEditText totalPriceTextInputEditText = findViewById(R.id.textInputEditText_total_price_add_estimate_line);
+                TextInputEditText weightTextInputEditText = findViewById(R.id.weightEditText_add_estimate_line);
+                TextInputEditText lengthTextInputEditText = findViewById(R.id.lengthEditText_add_estimate_line);
+                TextInputEditText widthTextInputEditText = findViewById(R.id.widthEditText_add_estimate_line);
+                TextInputEditText quantityTextInputEditText = findViewById(R.id.quantityEditText_add_estimate_line);
+                TextInputEditText totalTextInputEditText = findViewById(R.id.totalEditText_add_estimate_line);
+                TextInputEditText marginTextInputEditText = findViewById(R.id.marginEditText_add_estimate_line);
+                TextInputEditText netQuantityPlusMarginTextInputEditText = findViewById(R.id.netQuantityEditText_add_estimate_line);
+                TextInputEditText unitPriceTextInputEditText = findViewById(R.id.unitPriceEditText_add_estimate_line);
+                TextInputEditText totalPriceTextInputEditText = findViewById(R.id.totalPriceEditText_add_estimate_line);
 
                 String height = s.toString();
 
