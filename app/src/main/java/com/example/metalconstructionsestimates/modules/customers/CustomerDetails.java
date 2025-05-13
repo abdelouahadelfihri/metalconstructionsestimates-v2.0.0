8107ab080d3f218e40c7a173d0bbdff95cc8d483
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-05-13 19:14:37 +0100

    updates to CustomerDetails.java

--- app/src/main/java/com/example/metalconstructionsestimates/modules/customers/CustomerDetails.java
+++ app/src/main/java/com/example/metalconstructionsestimates/modules/customers/CustomerDetails.java
@@ -24,7 +24,7 @@ public class CustomerDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_details);
-        Toolbar toolbar = findViewById(R.id.toolbar_customer_details);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         String customerIdExtra = getIntent().getStringExtra("customerIdExtra");
