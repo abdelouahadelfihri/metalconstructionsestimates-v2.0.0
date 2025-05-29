fcd53cd1759cc8ad90b5ac84b8a4d90dd078353e
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-05-29 21:41:12 +0100

    updates to DBAdapter.java

--- app/src/main/java/com/example/metalconstructionsestimates/db/DBAdapter.java
+++ app/src/main/java/com/example/metalconstructionsestimates/db/DBAdapter.java
@@ -1365,7 +1365,7 @@ public class DBAdapter {
             db = helper.getReadableDatabase();
 
             String query = selectQuery + whereQuery;
-            query = query + " and amountPaid = allTaxIncludedTotal and amountPaid != 0";
+            query = query + " AND ABS(amountPaid - allTaxIncludedTotal) < 0.001 AND amountPaid != 0";
             Log.i("query", query);
             Cursor cursor = db.rawQuery(query, null);
             Estimate estimate;
