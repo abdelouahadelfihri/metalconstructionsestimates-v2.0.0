c36567ffd03cc4e56d926b3ea81847cf9b5b9ce0
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-05-30 20:11:18 +0100

    updates to DBAdapter.java

--- app/src/main/java/com/example/metalconstructionsestimates/db/DBAdapter.java
+++ app/src/main/java/com/example/metalconstructionsestimates/db/DBAdapter.java
@@ -1550,7 +1550,7 @@ public class DBAdapter {
 
             String query = SELECTQuery + WHEREQuery;
 
-            query = query + " amountPaid = 0.0";
+            query = query + " AND amountPaid BETWEEN -0.001 AND 0.001";
 
             Cursor cursor = db.rawQuery(query, null);
 
@@ -2161,7 +2161,7 @@ public class DBAdapter {
         try{
 
             db = helper.getReadableDatabase();
-            cursor = db.rawQuery("SELECT * FROM estimate WHERE amountPaid = 0.0",null);
+            cursor = db.rawQuery("SELECT * FROM estimate WHERE amountPaid BETWEEN -0.001 AND 0.001",null);
             while(cursor.moveToNext()){
                 Integer estimateId = cursor.getInt(0);
                 String doneIn = cursor.getString(1);
