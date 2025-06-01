8f3c8ebee1b21b8150b928c118c1dc15298d1aee
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-01 20:41:21 +0100

    updates to EstimateDetails.java

--- app/src/main/java/com/example/metalconstructionsestimates/modules/estimates/EstimateDetails.java
+++ app/src/main/java/com/example/metalconstructionsestimates/modules/estimates/EstimateDetails.java
@@ -569,10 +569,11 @@ public class EstimateDetails extends AppCompatActivity {
                 }
                 long diffInMillis = expirationDate.getTime() - issueDate.getTime();
                 long daysBetween = diffInMillis / (1000 * 60 * 60 * 24);
-                if(daysBetween < 1){
+                if(daysBetween <= 0){
                     Toast.makeText(getApplicationContext(), "Expiration date should be after the issue date", Toast.LENGTH_SHORT).show();
                     TextView expirationDateTextView = (TextView) findViewById(R.id.expirationDateValue);
                     expirationDateTextView.setText(R.string.expirationDate);
+                    expirationDateValue = "";
                 }
             }
         };
@@ -601,10 +602,11 @@ public class EstimateDetails extends AppCompatActivity {
                 }
                 long diffInMillis = expirationDate.getTime() - issueDate.getTime();
                 long daysBetween = diffInMillis / (1000 * 60 * 60 * 24);
-                if(daysBetween < 1){
+                if(daysBetween <= 0){
                     Toast.makeText(getApplicationContext(), "Expiration date should be after the issue date", Toast.LENGTH_SHORT).show();
                     TextView issueDateTextView = (TextView) findViewById(R.id.issueDateValue);
                     issueDateTextView.setText(R.string.issueDate);
+                    issueDateValue = "";
                 }
             }
         };
