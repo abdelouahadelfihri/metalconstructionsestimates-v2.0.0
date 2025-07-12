83e1a24e245cc8a0125344333a3c66ed85769b4b
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-12 11:47:48 +0100

    updates to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         )
         applicationId = "com.example.metalconstructionsestimates"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
