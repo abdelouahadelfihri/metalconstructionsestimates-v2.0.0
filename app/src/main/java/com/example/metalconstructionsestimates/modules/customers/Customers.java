574bb52838b079aa16a648ca75d092406ab07011
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-22 20:20:53 +0100

    updates to Customers.java

--- app/src/main/java/com/example/metalconstructionsestimates/modules/customers/Customers.java
+++ app/src/main/java/com/example/metalconstructionsestimates/modules/customers/Customers.java
@@ -38,12 +38,14 @@ public class Customers extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         getWindow().setStatusBarColor(Color.parseColor("#0066cc"));
         getWindow().setNavigationBarColor(Color.parseColor("#0066cc"));
+
         WindowInsetsControllerCompat insetsController =
                 new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
         insetsController.setAppearanceLightStatusBars(false);
+        insetsController.setAppearanceLightNavigationBars(false);
         super.onCreate(savedInstanceState);
         binding = ActivityCustomersBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
