bdf90fb5558ddb7a8f82b3654617ec22fe36d6c2
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-25 20:11:07 +0100

    updates to AddEstimateLine.java

--- app/src/main/java/com/example/metalconstructionsestimates/modules/estimateslines/AddEstimateLine.java
+++ app/src/main/java/com/example/metalconstructionsestimates/modules/estimateslines/AddEstimateLine.java
@@ -62,33 +62,8 @@ public class AddEstimateLine extends AppCompatActivity {
     String formattedTotalPrice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
-        getWindow().setStatusBarColor(Color.parseColor("#0066cc"));
-        getWindow().setNavigationBarColor(Color.parseColor("#0066cc"));
-        WindowInsetsControllerCompat insetsController =
-                new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
-        insetsController.setAppearanceLightStatusBars(false);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_estimate_line);
-        View statusBarSpacer = findViewById(R.id.statusBarSpacer);
-        View scrollContent = findViewById(R.id.scrollContent);
-        ViewCompat.setOnApplyWindowInsetsListener(statusBarSpacer, (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            ViewGroup.LayoutParams params = v.getLayoutParams();
-            params.height = systemBars.top;
-            v.setLayoutParams(params);
-            return insets;
-        });
-        ViewCompat.setOnApplyWindowInsetsListener(scrollContent, (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(
-                    v.getPaddingLeft(),
-                    v.getPaddingTop(),
-                    v.getPaddingRight(),
-                    systemBars.bottom // ⬅️ This prevents buttons from being hidden by navigation bar
-            );
-            return insets;
-        });
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
