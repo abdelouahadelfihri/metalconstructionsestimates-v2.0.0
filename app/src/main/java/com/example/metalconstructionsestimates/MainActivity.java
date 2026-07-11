a363a48ae3f0125bbeee76329533c6caad46d0d8
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-07-11 19:12:36 +0100

    updates to MainActivity.java

--- app/src/main/java/com/example/metalconstructionsestimates/MainActivity.java
+++ app/src/main/java/com/example/metalconstructionsestimates/MainActivity.java
@@ -98,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         imageViewEstimates.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
