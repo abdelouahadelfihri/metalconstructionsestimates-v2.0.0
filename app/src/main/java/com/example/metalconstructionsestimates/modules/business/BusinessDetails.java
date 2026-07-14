83af56e909a446541dd7bc888c61909e3a9e1c3d
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-07-14 19:58:47 +0100

    updates to activity_business_details.xml, BusinessDetails.java

--- app/src/main/java/com/example/metalconstructionsestimates/modules/business/BusinessDetails.java
+++ app/src/main/java/com/example/metalconstructionsestimates/modules/business/BusinessDetails.java
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.example.metalconstructionsestimates.R;
 import com.example.metalconstructionsestimates.database.DBAdapter;
+import com.example.metalconstructionsestimates.models.Business;
 import com.example.metalconstructionsestimates.models.Customer;
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -18,50 +19,48 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class BusinessDetails extends AppCompatActivity {
-    Customer customer;
+    Business business;
     DBAdapter dbAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_details);
+        setContentView(R.layout.activity_business_details);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         dbAdapter = new DBAdapter(getApplicationContext());
-        customer = dbAdapter.getBusi(customerId);
+        business = dbAdapter.getBusiness();
 
-        AtomicReference<TextInputEditText> customerIdTextInputEditText = new AtomicReference<>(findViewById(R.id.customerIdEditText_customer_details));
-        TextInputEditText customerNameTextInputEditText = findViewById(R.id.nameEditText_customer_details);
-        TextInputEditText customerEmailTextInputEditText = findViewById(R.id.emailEditText_customer_details);
-        TextInputEditText customerPhoneTextInputEditText = findViewById(R.id.telephoneEditText_customer_details);
-        TextInputEditText customerMobileTextInputEditText = findViewById(R.id.mobileEditText_customer_details);
-        TextInputEditText customerFaxTextInputEditText = findViewById(R.id.faxEditText_customer_details);
-        TextInputEditText customerAddressTextInputEditText = findViewById(R.id.addressEditText_customer_details);
+        TextInputEditText businessNameTextInputEditText = findViewById(R.id.nameEditText_business_details);
+        TextInputEditText businessEmailTextInputEditText = findViewById(R.id.emailEditText_business_details);
+        TextInputEditText businessPhoneTextInputEditText = findViewById(R.id.telephoneEditText_business_details);
+        TextInputEditText businessMobileTextInputEditText = findViewById(R.id.mobileEditText_business_details);
+        TextInputEditText businessFaxTextInputEditText = findViewById(R.id.faxEditText_business_details);
+        TextInputEditText businessAddressTextInputEditText = findViewById(R.id.addressEditText_business_details);
 
-        customerIdTextInputEditText.get().setText(customerId.toString());
-        customerNameTextInputEditText.setText(customer.getName());
-        customerEmailTextInputEditText.setText(customer.getEmail());
-        customerPhoneTextInputEditText.setText(customer.getTelephone());
-        customerMobileTextInputEditText.setText(customer.getMobile());
-        customerFaxTextInputEditText.setText(customer.getFax());
-        customerAddressTextInputEditText.setText(customer.getAddress());
 
-        Button deleteCustomer = findViewById(R.id.buttonDelete_customer_details);
-        Button updateCustomer = findViewById(R.id.buttonEdit_customer_details);
+        businessNameTextInputEditText.setText(business.getName());
+        businessEmailTextInputEditText.setText(business.getEmail());
+        businessPhoneTextInputEditText.setText(business.getPhone());
+        businessMobileTextInputEditText.setText(business.getMobile());
+        businessFaxTextInputEditText.setText(business.getFax());
+        businessAddressTextInputEditText.setText(business.getAddress());
+
+        Button deleteCustomer = findViewById(R.id.buttonDelete_business_details);
+        Button updateCustomer = findViewById(R.id.buttonEdit_business_details);
 
 
         deleteCustomer.setOnClickListener(view -> {
             AlertDialog.Builder alertDelete = new AlertDialog.Builder(BusinessDetails.this);
             alertDelete.setTitle("Delete Confirmation");
-            alertDelete.setMessage("Do you really want to delete the customer ?");
+            alertDelete.setMessage("Do you really want to delete the business ?");
             alertDelete.setPositiveButton(android.R.string.ok, (dialog, which) -> {
                 // continue with delete
-                customer = new Customer();
-                customerIdTextInputEditText.set(findViewById(R.id.customerIdEditText_customer_details));
-                dbAdapter.deleteCustomer(Integer.parseInt(customerIdTextInputEditText.get().getText().toString()));
+                business = new Customer();
+                dbAdapter.deleteCustomer(Integer.parseInt(businessIdTextInputEditText.get().getText().toString()));
                 Toast deletesuccess = Toast.makeText(getApplicationContext(), "Suppression du pièce métallique a été effectuée avec succés", Toast.LENGTH_LONG);
                 deletesuccess.show();
                 if(dbAdapter.retrieveCustomers().isEmpty()){
@@ -80,54 +79,54 @@ public class BusinessDetails extends AppCompatActivity {
         updateCustomer.setOnClickListener(view -> {
             AlertDialog.Builder alertUpdate = new AlertDialog.Builder(BusinessDetails.this);
             alertUpdate.setTitle("Confirm Update");
-            alertUpdate.setMessage("Do you really want to update customer ?");
+            alertUpdate.setMessage("Do you really want to update business ?");
             alertUpdate.setPositiveButton(android.R.string.ok, (dialog, which) -> {
                 // continue with delete
-                customer = new Customer();
+                business = new Customer();
 
-                if (!customerIdTextInputEditText.get().getText().toString().isEmpty()) {
-                    customer.setId(Integer.parseInt(customerIdTextInputEditText.get().getText().toString()));
+                if (!businessIdTextInputEditText.get().getText().toString().isEmpty()) {
+                    business.setId(Integer.parseInt(businessIdTextInputEditText.get().getText().toString()));
                 } else {
-                    customer.setId(Integer.valueOf(""));
+                    business.setId(Integer.valueOf(""));
                 }
 
-                if (!customerNameTextInputEditText.getText().toString().isEmpty()) {
-                    customer.setName(customerNameTextInputEditText.getText().toString());
+                if (!businessNameTextInputEditText.getText().toString().isEmpty()) {
+                    business.setName(businessNameTextInputEditText.getText().toString());
                 } else {
-                    customer.setName("");
+                    business.setName("");
                 }
 
-                if (!customerEmailTextInputEditText.getText().toString().isEmpty()) {
-                    customer.setEmail(customerEmailTextInputEditText.getText().toString());
+                if (!businessEmailTextInputEditText.getText().toString().isEmpty()) {
+                    business.setEmail(businessEmailTextInputEditText.getText().toString());
                 } else {
-                    customer.setEmail("");
+                    business.setEmail("");
                 }
 
-                if (!customerPhoneTextInputEditText.getText().toString().isEmpty()) {
-                    customer.setTelephone(customerPhoneTextInputEditText.getText().toString());
+                if (!businessPhoneTextInputEditText.getText().toString().isEmpty()) {
+                    business.setTelephone(businessPhoneTextInputEditText.getText().toString());
                 } else {
-                    customer.setTelephone("");
+                    business.setTelephone("");
                 }
 
-                if (!customerMobileTextInputEditText.getText().toString().isEmpty()) {
-                    customer.setMobile(customerMobileTextInputEditText.getText().toString());
+                if (!businessMobileTextInputEditText.getText().toString().isEmpty()) {
+                    business.setMobile(businessMobileTextInputEditText.getText().toString());
                 } else {
-                    customer.setMobile("");
+                    business.setMobile("");
                 }
 
-                if (!customerFaxTextInputEditText.getText().toString().isEmpty()) {
-                    customer.setFax(customerFaxTextInputEditText.getText().toString());
+                if (!businessFaxTextInputEditText.getText().toString().isEmpty()) {
+                    business.setFax(businessFaxTextInputEditText.getText().toString());
                 } else {
-                    customer.setFax("");
+                    business.setFax("");
                 }
 
-                if (!customerAddressTextInputEditText.getText().toString().isEmpty()) {
-                    customer.setAddress(customerAddressTextInputEditText.getText().toString());
+                if (!businessAddressTextInputEditText.getText().toString().isEmpty()) {
+                    business.setAddress(businessAddressTextInputEditText.getText().toString());
                 } else {
-                    customer.setAddress("");
+                    business.setAddress("");
                 }
 
-                dbAdapter.updateCustomer(customer);
+                dbAdapter.updateCustomer(business);
                 Toast updateSuccessToast = Toast.makeText(getApplicationContext(), "Modification du client a été effectué avec succés", Toast.LENGTH_LONG);
                 updateSuccessToast.show();
             });
